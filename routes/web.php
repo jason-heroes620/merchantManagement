814492c35efeee3af878dbe95206385185812ed5
jason-heroes620 <jason.w@heroes.my>
2024-08-21 18:12:10 +0800

    web route artisan call reverb

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::group(['middleware' => ['auth']], function () {
 // });
 
 Route::get('/reverb-start', function () {
-    Artisan::call('reverb:start');
+    Artisan::call('reverb:start --port=8080');
     return "Reverb started";
 });
 
